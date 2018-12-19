402cb5f6b207c56168a77bb107e664d8b05a95c0
Daniel Bowen <danielrbowen@live.com>
2018-12-18 18:48:16 -0700

    Fixed add and get activities. Added an editor config file. Doesn't get the activities after the post.

--- ActivityWeatherSchedulerBlazor.Server/Controllers/WeatherForecastController.cs
+++ ActivityWeatherSchedulerBlazor.Server/Controllers/WeatherForecastController.cs
@@ -1,60 +1,58 @@
-﻿using ActivityWeatherSchedulerBlazor.Server.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ActivityWeatherSchedulerBlazor.Server.Data;
 using ActivityWeatherSchedulerLibraryNETStandard;
 using ActivityWeatherSchedulerLibraryNETStandard.Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ActivityWeatherSchedulerBlazor.Server.Controllers
 {
-    [Route("api/[controller]")]
-    public class WeatherForecastController : Controller
-    {
-        private ActivityContext ActivityDbContext { get; }
+	[Route("api/[controller]")]
+	public class WeatherForecastController : Controller
+	{
+		private ActivityContext ActivityDbContext { get; }
 
-        public WeatherForecastController(ActivityContext activityContext)
-        {
-            ActivityDbContext = activityContext ?? throw new ArgumentNullException(nameof(activityContext));
-        }
+		public WeatherForecastController(ActivityContext activityContext)
+		{
+			ActivityDbContext = activityContext ?? throw new ArgumentNullException(nameof(activityContext));
+		}
 
-        [HttpGet("[action]/{zip}")]
-        public IEnumerable<WeatherForecast> FiveDayWeatherForecast(string zip)
-        {
-            if (string.IsNullOrWhiteSpace(zip))
-            {
-                return null;
-            }
+		[HttpGet("[action]/{zip}")]
+		public IEnumerable<WeatherForecast> FiveDayWeatherForecast(string zip)
+		{
+			if (string.IsNullOrWhiteSpace(zip))
+			{
+				return null;
+			}
 
-            return GetWeather.FiveDayAround1500Forecast(zip);
-        }
+			return GetWeather.FiveDayAround1500Forecast(zip);
+		}
 
-        [HttpGet("[action]/{latitude}&{longitude}")]
-        public IEnumerable<WeatherForecast> FiveDayWeatherForecast(double latitude, double longitude)
-        {
-            return GetWeather.FiveDayAround1500Forecast(latitude, longitude);
-        }
+		[HttpGet("[action]/{latitude}&{longitude}")]
+		public IEnumerable<WeatherForecast> FiveDayWeatherForecast(double latitude, double longitude)
+		{
+			return GetWeather.FiveDayAround1500Forecast(latitude, longitude);
+		}
 
-        [HttpPost("[action]")]
-        public void AddActivity(Activity activity)
-        {
-            ActivityDbContext.Activities.Add(activity);
-        }
+		[HttpPost("[action]")]
+		public async Task AddActivity([FromBody]Activity activity)
+		{
+			ActivityDbContext.Activities.Add(activity);
+			await ActivityDbContext.SaveChangesAsync();
+		}
 
-        [HttpGet("[action]/{email}")]
-        public IEnumerable<Activity> GetActivities(string email)
-        {
-            var activities = ActivityDbContext.Activities.Where(activity => activity.Email == email);
-            if (activities != null && activities.Any())
-            {
-                return activities;
-            }
-            else
-            {
-                return new Activity[0];
-            }
-        }
-    }
+		[HttpGet("[action]/{email}")]
+		public async Task<IList<Activity>> GetActivities(string email)
+		{
+			var activities = await ActivityDbContext.Activities
+				.Where(activity => activity.Email == email)
+				.ToListAsync();
+			return activities;
+		}
+	}
 }
