34cfe80efa771b39b7ec87d168d2d12c9fa91c72
Daniel Bowen <danielrbowen@live.com>
2018-12-22 19:45:07 -0700

    StateHasChanged is removed. And added DualMode code changes. https://github.com/Suchiman/BlazorDualMode

--- ActivityWeatherSchedulerBlazor.Client/Startup.cs
+++ ActivityWeatherSchedulerBlazor.Client/Startup.cs
@@ -1,17 +1,35 @@
+using System;
+using System.Linq;
+using System.Net.Http;
 using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Blazor.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ActivityWeatherSchedulerBlazor.Client
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			// Server Side Blazor doesn't register HttpClient by default
+			if (!services.Any(x => x.ServiceType == typeof(HttpClient)))
+			{
+				// Setup HttpClient for server side in a client side compatible fashion
+				services.AddScoped<HttpClient>(s =>
+				{
+					// Creating the URI helper needs to wait until the JS Runtime is initialized, so defer it.
+					var uriHelper = s.GetRequiredService<IUriHelper>();
+					return new HttpClient
+					{
+						BaseAddress = new Uri(uriHelper.GetBaseUri())
+					};
+				});
+			}
+		}
 
-        public void Configure(IBlazorApplicationBuilder app)
-        {
-            app.AddComponent<App>("app");
-        }
-    }
+		public void Configure(IBlazorApplicationBuilder app)
+		{
+			app.AddComponent<App>("app");
+		}
+	}
 }
--- ActivityWeatherSchedulerBlazor.Server/Startup.cs
+++ ActivityWeatherSchedulerBlazor.Server/Startup.cs
@@ -11,55 +11,60 @@ using System.Net.Mime;
 
 namespace ActivityWeatherSchedulerBlazor.Server
 {
-    public class Startup
-    {
-        public IConfiguration Configuration { get; }
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
 
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ActivityContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ActivityContext>(options =>
+			options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+			services.AddMvc();
 
-            services.AddResponseCompression(options =>
-            {
-                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
-                {
-                    MediaTypeNames.Application.Octet,
-                    WasmMediaTypeNames.Application.Wasm,
-                });
-            });
-        }
+			// Adds the Server-Side Blazor services, and those registered by the app project's startup.
+			// This doesn't hurt client side
+			services.AddServerSideBlazor<Client.Startup>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ActivityContext activityContext)
-        {
-            app.UseResponseCompression();
+			services.AddResponseCompression(options =>
+			{
+				options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+				{
+					MediaTypeNames.Application.Octet,
+					WasmMediaTypeNames.Application.Wasm,
+				});
+			});
+		}
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ActivityContext activityContext)
+		{
+			app.UseResponseCompression();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(name: "default", template: "{controller}/{action}/{id?}");
-            });
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
 
-            activityContext.Database.EnsureCreated();
-            app.UseBlazor<Client.Startup>();
-        }
-    }
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(name: "default", template: "{controller}/{action}/{id?}");
+			});
+
+			activityContext.Database.EnsureCreated();
+			// This will also serve a client side app, it just registers SignalR in addition
+			app.UseServerSideBlazor<Client.Startup>();
+		}
+	}
 }
